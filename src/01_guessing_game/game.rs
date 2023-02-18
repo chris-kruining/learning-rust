5f7255af09b6418297968cb2031d7b1640e0fe82
Chris Kruining <chris@kruining.eu>
2023-02-18 18:49:41 +0100

    finished the first guide with guessing game

---
+++ src/01_guessing_game/game.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+pub fn run() {
+    
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Psst, here is a hint, it is: {}", secret_number);
+
+    loop {
+        println!("\nPlease input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess = guess.trim();
+        let guess = match guess.to_string().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("'{}' is nog a valid number", guess);
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("You got it");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#[path="01_guessing_game/game.rs"]
+mod guessing_game;
+
+fn main() {
+    guessing_game::run();
+}
